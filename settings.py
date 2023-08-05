704292b33616f38fe8ccdb43080d5011a11a24df
Alberto <rinconegocio@gmail.com>
2023-08-04 23:45:45 -0300

    clean settings

--- settings.py
+++ settings.py
@@ -1,2 +1,21 @@
-BOT_API_KEY=""
 NEWS_API_KEY=""
+BOT_API_KEY=""
+
+VALID_TOPICS = [
+    "blockchain",
+    "earnings",
+    "ipo",
+    "mergers_and_acquisitions",
+    "financial_markets",
+    "economy_fiscal",
+    "economy_monetary",
+    "economy_macro",
+    "energy_transportation",
+    "finance",
+    "life_sciences",
+    "manufacturing",
+    "real_estate",
+    "retail_wholesale",
+    "technology"
+]
+
